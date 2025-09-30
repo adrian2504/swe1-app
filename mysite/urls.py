dbc8cfe1d1fc75569af694cb06f4ebf27be3fb7c
adrian2504 <adriandsouza2504@gmail.com>
2025-09-30 14:21:22 -0400

    Enable Whitenoise for static files and add homepage

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Welcome to my Django App on AWS!</h1><p>Go to <a href='/polls/'>Polls</a> or <a href='/admin/'>Admin</a>.</p>")
+
 
 urlpatterns = [
+    path("", home, name="home"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
