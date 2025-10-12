1f295c0d7ded4084b432fdd9649082ac221f9f11
adrian2504 <adriandsouza2504@gmail.com>
2025-10-12 16:07:29 -0400

    Fix test route and ensure Travis CI passes

--- polls/tests.py
+++ polls/tests.py
@@ -10,7 +10,7 @@ from .models import Question
 class SmokeTest(TestCase):
     def test_homepage_loads(self):
         """Simple test to ensure the homepage returns 200."""
-        response = self.client.get(reverse("index"))
+        response = self.client.get(reverse("polls:IndexView"))
         self.assertEqual(response.status_code, 200)
 
 
@@ -34,11 +34,11 @@ class QuestionModelTests(TestCase):
         self.assertIs(old_question.was_published_recently(), False)
 
 
-def test_was_published_recently_with_recent_question(self):
-    """
-    was_published_recently() returns True for questions whose pub_date
-    is within the last day.
-    """
-    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-    recent_question = Question(pub_date=time)
-    self.assertIs(recent_question.was_published_recently(), True)
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.db.models import F
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
+
+
 
 
 class IndexView(generic.ListView):
